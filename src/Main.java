27fca06d210c67b8e8eeb0058909eaa4881483d4
Yaroslav Guz <aroslavguz@gmail.com>
2025-03-25 16:04:17 +0200

    Add display of selected file to image panel

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 
 public class Main {
+    private static JLabel imageLabel;
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("Color models!");
         frame.setLayout(new BorderLayout());
@@ -17,6 +17,7 @@ public class Main {
 
     private static void initializeUI(JFrame frame) {
         initializeInputPanel(frame);
+        initializeImagePanel(frame);
     }
 
     private static void initializeInputPanel(JFrame frame) {
@@ -30,10 +31,24 @@ public class Main {
             if (returnValue == JFileChooser.APPROVE_OPTION) {
                 File selectedFile = fileChooser.getSelectedFile();
                 System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+                displayImage(selectedFile);
             }
         });
 
         inputPanel.add(uploadButton);
         frame.add(inputPanel, BorderLayout.NORTH);
+    }
+
+    private static void initializeImagePanel(JFrame frame) {
+        JPanel imagePanel = new JPanel();
+        imageLabel = new JLabel();
+        imagePanel.add(imageLabel);
+        frame.add(imagePanel, BorderLayout.CENTER);
+    }
+
+    private static void displayImage(File file) {
+        ImageIcon imageIcon = new ImageIcon(file.getAbsolutePath());
+        Image image = imageIcon.getImage().getScaledInstance(800, 800, Image.SCALE_SMOOTH);
+        imageLabel.setIcon(new ImageIcon(image));
     }
 }
