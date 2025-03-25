a274b5492638c040ab2e6af7f52a444ad35f967b
Yaroslav Guz <aroslavguz@gmail.com>
2025-03-25 15:51:04 +0200

    Create main program frame

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        JFrame frame = new JFrame("Color models!");
+        frame.setLayout(new BorderLayout());
+        initializeUI(frame);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+    }
+
+    private static void initializeUI(JFrame frame) {
     }
 }
