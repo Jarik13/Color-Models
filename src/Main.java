3cbe6e75f9fff46adeff1ac0e90e8392873978ee
Yaroslav Guz <aroslavguz@gmail.com>
2025-03-27 18:56:05 +0200

    Add selecting rectangle to image panel

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@ import managers.ColorModelManager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    private static JLabel imageLabel;
     private static BufferedImage originalImage;
+    private static BufferedImage displayedImage;
+    private static Rectangle selectionRect = new Rectangle();
+    private static Point2D.Double startPoint = null;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Color models!");
@@ -39,8 +44,8 @@ public class Main {
                 File selectedFile = fileChooser.getSelectedFile();
                 try {
                     originalImage = javax.imageio.ImageIO.read(selectedFile);
-                    BufferedImage hsvImage = ColorModelManager.convertToHSV(originalImage);
-                    displayImage(hsvImage);
+                    displayedImage = originalImage;
+                    repaintImage();
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
@@ -49,17 +54,15 @@ public class Main {
 
         convertHSVButton.addActionListener(e -> {
             if (originalImage != null) {
-                BufferedImage hsvImage = ColorModelManager.convertToHSV(originalImage);
-                ImageIcon hsvIcon = new ImageIcon(hsvImage.getScaledInstance(800, 800, Image.SCALE_SMOOTH));
-                imageLabel.setIcon(hsvIcon);
+                displayedImage = ColorModelManager.convertToHSV(originalImage);
+                repaintImage();
             }
         });
 
         convertRGBButton.addActionListener(e -> {
             if (originalImage != null) {
-                BufferedImage rgbImage = ColorModelManager.convertToRGB(originalImage);
-                ImageIcon rgbIcon = new ImageIcon(rgbImage.getScaledInstance(800, 800, Image.SCALE_SMOOTH));
-                imageLabel.setIcon(rgbIcon);
+                displayedImage = ColorModelManager.convertToRGB(originalImage);
+                repaintImage();
             }
         });
 
@@ -70,14 +73,59 @@ public class Main {
     }
 
     private static void initializeImagePanel(JFrame frame) {
-        JPanel imagePanel = new JPanel();
-        imageLabel = new JLabel();
-        imagePanel.add(imageLabel);
+        JPanel imagePanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                if (displayedImage != null) {
+                    g.drawImage(displayedImage, 0, 0, this);
+
+                    if (selectionRect.width > 0 && selectionRect.height > 0) {
+                        g.setColor(new Color(0, 0, 255, 50));
+                        g.fillRect(selectionRect.x, selectionRect.y, selectionRect.width, selectionRect.height);
+                        g.setColor(Color.BLUE);
+                        g.drawRect(selectionRect.x, selectionRect.y, selectionRect.width, selectionRect.height);
+                    }
+                }
+            }
+        };
+
+        imagePanel.setPreferredSize(new Dimension(800, 800));
+
+        imagePanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                startPoint = new Point2D.Double(e.getX(), e.getY());
+                selectionRect.setBounds(e.getX(), e.getY(), 0, 0);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                selectionRect.setBounds(0, 0, 0, 0);
+                imagePanel.repaint();
+            }
+        });
+
+        imagePanel.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                int x = (int) Math.min(startPoint.getX(), e.getX());
+                int y = (int) Math.min(startPoint.getY(), e.getY());
+                int width = Math.abs((int) (startPoint.getX() - e.getX()));
+                int height = Math.abs((int) (startPoint.getY() - e.getY()));
+                selectionRect.setBounds(x, y, width, height);
+                imagePanel.repaint();
+            }
+        });
+
         frame.add(imagePanel, BorderLayout.CENTER);
     }
 
-    private static void displayImage(BufferedImage image) {
-        ImageIcon imageIcon = new ImageIcon(image.getScaledInstance(800, 800, Image.SCALE_SMOOTH));
-        imageLabel.setIcon(imageIcon);
+    private static void repaintImage() {
+        for (Window window : JFrame.getWindows()) {
+            if (window instanceof JFrame) {
+                window.repaint();
+            }
+        }
     }
 }
